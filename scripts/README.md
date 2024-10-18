e0eb596449fe1f8bfb99dc4e3dc6e536475b8999
Nathan Heidt <nathan@nathan>
2024-10-18 15:11:39 -0700

    Swithced over to nmcli which controls wifi successfully. Added untested code for M117 command. Serves webpage and can connect to wifi properly. Updated Readme.

--- scripts/README.md
+++ scripts/README.md
@@ -1,8 +1,32 @@
 ### Setup
 
-Much of this code was vendored in from PyWifiControl (https://github.com/emlid/pywificontrol/tree/master) as well as sysdmanager (https://github.com/emlid/systemd-manager/blob/master/sysdmanager/systemd_manager.py) as both of these libraries are no longer maintained.
+#### Setup AP
 
+The first step is to enable the SoftAP that comes with the beaglebone. It can run simultanously in client and hotspot mode.
 
-sudo apt install dbus libdbus-glib-1-dev libdbus-1-dev python3-dbus hostapd
+`sudo cp hostapd.conf /etc/hostapd/hostapd.conf`
 
-need to copy hostapd.conf to /etc/hostapd/hostapd.conf
+#### Enable permissions for WiFi control
+
+Second step is to create a polkit rule for netdev so we don't have to run as root.  Create a file called `10-networkmanager.riles` to `/etc/polkit-1/rules.d` and add the following:
+
+```
+polkit.addRule(function(action, subject) {
+	if (subject.isInGroup("netdev")) {
+    	if (action.id == "org.freedesktop.NetworkManager.settings.modify.own" ||
+        	action.id == "org.freedesktop.NetworkManager.settings.modify.system" ||
+        	action.id == "org.freedesktop.NetworkManager.network-control" ||
+        	action.id == "org.freedesktop.NetworkManager.enable-disable-wifi") {
+        	return polkit.Result.YES;
+    	}
+	}
+});
+```
+
+Finally add the user to the netdev group: `usermod -a -G netdev YOUR_USERNAME` where YOUR_USERNAME is whatever you have setup to be the main user on the beaglebone.
+
+#### Set the Hostname
+
+If you want to set a custom mdns hostname, simple change the name in `/etc/hostname` which is typically beagle by default.
+
+### Configure nginx to allow octoprint and our server to run side by side
--- scripts/initial_config.py
+++ scripts/initial_config.py
@@ -1,86 +1,60 @@
-from wpasupplicant import WpaSupplicant
-from utils.dbuswpasupplicant import PropertyError
-import time
-from hostapd import HostAP
+from nicegui import ui
+from nicegui.events import ValueChangeEventArguments
+import nmcli
+import requests
+import os
 
+API_KEY = os.environ['OCTOPRINT_API_KEY']
 
-class WifiManager:
-    # TODO(heidt) should do system query to get interfaces
-    def __init__(self, 
-                 interface="wlp7s0",
-                 hostapd_config="/etc/hostapd/hostapd.conf",
-                 wpas_config="/etc/wpa_supplicant/wpa_supplicant.conf",
-                 p2p_config="/etc/wpa_supplicant/p2p_supplicant.conf",
-                 hostname_config='/etc/hostname'):
-        self.interface = interface
-        self.hotspot = HostAP(interface, hostapd_config, hostname_config)
-        self.wpasupplicant = WpaSupplicant(interface, wpas_config, p2p_config)
-        
-    def get_current_network_ssid(self):
-        try:
-            return self.wpasupplicant.get_current_network_ssid()
-        except PropertyError:
-            return None
-        
-    def is_connected(self):
-        return self.get_current_network_ssid()
+class ConfigPage:
+    def __init__(self):
+        self.ssid = ""
+        self.password = ""
 
-    def wait_connected(self, seconds):
-        for i in range(seconds):
-            if self.is_connected():
+def is_wifi_connected():
+    for dev in nmcli.device():
+        if dev.device == "wlan0":
+            # there can be a variety of connected tags e.g. connected (managed) and the word connected
+            # is in disconnected
+            if "connected" in dev.state and not "disconnected" in dev.state:
                 return True
-            time.sleep(1)
-        return False
+    return False
 
-    def scan_all_SSIDs(self):
-        self.wpasupplicant.scan()
-        return self.wpasupplicant.get_scan_results()
-    
-    def start_host_mode(self, SSID_name="d3d-pro-printer"):
-        if not self.hotspot.started():
-            self.wpasupplicant.stop()
-            self.hotspot.set_hostap_name(SSID_name)
-            self.hotspot.start()
-        return True
-    
-    def stop_host_mode(self):
-        if self.hotspot.started():
-            self.hotspot.stop()
-            self.wpasupplicant.start()
-        return True
+def send_octopi_lcd_text(text):
+    api_url = f"http://octoUrl/api/printer/command?apikey={API_KEY}"
+    lcd_command = {
+        "command": f"M117 {text}"
+    }
+    response = requests.post(api_url, json=lcd_command)
+
+def serve(wifinames):
+    # TODO(Heidt) should rescan on dropdown
+    config = ConfigPage()
+
+    def show(event: ValueChangeEventArguments):
+        name = type(event.sender).__name__
+        ui.notify(f"attempting to connect to {config.ssid}")
+        nmcli.device.wifi_connect(ssid=config.ssid, password=config.password, wait=10)
+        # TODO(Heidt) probably want to make sure it's the right wifi!
+        if is_wifi_connected():
+            ui.notify("Connected to wifi")
+        else:
+            ui.notify("Failed to connect to wifi")
+
+    ui.select(wifinames, value=wifinames[0]).bind_value(config, "ssid")
+    ui.input("password", password=True).bind_value(config, "password")
+    ui.button("Button", on_click=show)
+
+    ui.run()
+
 
 def main():
-    # check to see if wifi is connected
-    # wait for 30 seconds or some amount of time
-    # if wifi still isn't connected, search for all nearby SSIDs
-    # enter AP mode
-    # start server to select SSID and enter password
-    # once the user hits connect, enter client mode and connect to wifi
-    pass
-
-    manager = WifiManager()
-    print("Manager started")
-    if manager.is_connected():
-        ssid = manager.get_current_network_ssid()
-        print(f"Wifi already connected to {ssid}")
-        return True
-    else:
-        print("waiting for wifi connection...")
-        succuss = manager.wait_connected(3)
-        if succuss:
-            return True
-        else:
-            print("Wifi connection failed")
-    
-    nearby_ssids = manager.scan_all_SSIDs()
+    nearby_ssids = [network.ssid for network in nmcli.device.wifi()]
     print(f"Found nearby SSIDs")
     for ssid in nearby_ssids:
-        print(f"{ssid['ssid']}")
-    
-    print("entering hostmode")
-    manager.start_host_mode() 
+       print(f"{ssid}")
 
-    #manager.stop_host_mode()
+    serve(nearby_ssids)
 
-if __name__ == "__main__":
+if __name__ in {"__main__", "__mp_main__"}:
     main()
